a3ecf68c0e907f6f1b0646df63f84d0981985ffb
OskarOtt <oskar.ott@icloud.com>
2026-09-01 22:37:44 +0200

    nybakcnedurl

--- src/env.ts
+++ src/env.ts
@@ -2,4 +2,4 @@ export const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL ??
   (import.meta.env.DEV
     ? "http://localhost:8080"
-    : "https://webshop-template-backend.onrender.com");
+    : "https://webshop-api.oskott.com");
